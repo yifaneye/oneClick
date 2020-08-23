942b6a961e547332ff514b60fe76cea2df20e3b8
Yifan Ai <ayfallen@hotmail.com>
2020-08-23 22:56:17 +1000

    Initial Commit

---
+++ oneClick/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  oneClick
+//
+//  Created by Yifan Ai on 23/8/20.
+//  Copyright © 2020 Yifan Ai. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ oneClick/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  oneClick
+//
+//  Created by Yifan Ai on 23/8/20.
+//  Copyright © 2020 Yifan Ai. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
